da0f7d84db47e9d8f5e521380876585d2921952a
tranm <tranm@mail.nih.gov>
2009-04-21 22:36:52 +0000

    not using helper service

--- trunk/services/caGeneFilter/caGrid/CaGeneFilter/src/org/bioconductor/packages/caGeneFilter/context/service/globus/resource/CaGeneFilterServiceContextResource.java
+++ trunk/services/caGeneFilter/caGrid/CaGeneFilter/src/org/bioconductor/packages/caGeneFilter/context/service/globus/resource/CaGeneFilterServiceContextResource.java
@@ -16,25 +16,25 @@ import org.cagrid.transfer.context.service.helper.TransferServiceHelper;
 import org.cagrid.transfer.descriptor.DataDescriptor;
 
 
-/**
+/** 
  * The implementation of this CaGeneFilterServiceContextResource type.
- *
+ * 
  * @created by Introduce Toolkit version 1.2
- *
+ * 
  */
-public class CaGeneFilterServiceContextResource extends CaGeneFilterServiceContextResourceBase implements org.bioconductor.packages.helper.common.HelperServiceContextResourceI{
+public class CaGeneFilterServiceContextResource extends CaGeneFilterServiceContextResourceBase {
 	private static final int WAITING_TIME_OUT = 60000 * 3; // 3 mins
-
-
+	
+	
 	private Object m_mutexObj = new Object();
 	private boolean m_fileReady = false;
 	private java.util.ArrayList<FileReference> m_fromClientFileReferenceList = new java.util.ArrayList<FileReference>();
-
+	
 	public org.bioconductor.cagrid.data.ManufacturerFileReferences setDataFilesInput(final org.bioconductor.packages.rservices.FileReferences fileReferences) throws RemoteException
 	{
 		// new set, clear the old one:
 		m_fromClientFileReferenceList.clear();
-
+		
 		try {
 		  // create a data descriptor for upload data.
 		  DataDescriptor dd = new DataDescriptor(null, "caArrayQualityMetrics file transfer.");
@@ -48,7 +48,7 @@ public class CaGeneFilterServiceContextResource extends CaGeneFilterServiceConte
 
 				 synchronized(m_mutexObj) {
 					 // let's get some info out of TransferServiceContextResource:
-					 m_fileReady = false;
+					 m_fileReady = false;					 
 
 					 try {
 						  TransferServiceContextClient transSrvCxtClient = new TransferServiceContextClient(transCxtEPR);
@@ -62,7 +62,7 @@ public class CaGeneFilterServiceContextResource extends CaGeneFilterServiceConte
 
 						  m_fromClientFileReferenceList.add(newFileRef);
 
-						  m_fileReady = true;
+						  m_fileReady = true;						  
 					  }
 					catch(Exception ew) {
 						ew.printStackTrace();
@@ -74,7 +74,7 @@ public class CaGeneFilterServiceContextResource extends CaGeneFilterServiceConte
 
 		  org.bioconductor.cagrid.statefulservices.CaGridFileReference[] cagridFileReferenceArr =
 			    new org.bioconductor.cagrid.statefulservices.CaGridFileReference[fileReferences.size()];
-
+			  	
 
 		  for(int i = 0; i < cagridFileReferenceArr.length; i++) {
 			  CaGeneFilterDataStagedCallback callback = new CaGeneFilterDataStagedCallback();
@@ -82,12 +82,12 @@ public class CaGeneFilterServiceContextResource extends CaGeneFilterServiceConte
 			  // create the transfer resource that will handle receiving the data and
 			  // return the reference to the user
 			  org.cagrid.transfer.context.stubs.types.TransferServiceContextReference transCxtRef = TransferServiceHelper.createTransferContext(dd, callback);
-			  EndpointReferenceType eprType = transCxtRef.getEndpointReference();
+			  EndpointReferenceType eprType = transCxtRef.getEndpointReference(); 
 			  callback.transCxtEPR = eprType;
-
-			  FileReference fileRef = fileReferences.getFileReferences().get(i);
+			  
+			  FileReference fileRef = fileReferences.getFileReferences().get(i); 
 			  callback.fileRef = fileRef;
-
+			  
 			  cagridFileReferenceArr[i] = new org.bioconductor.cagrid.statefulservices.CaGridFileReference(eprType);
 			  cagridFileReferenceArr[i].setUrl(fileRef.getUrl());
 			  cagridFileReferenceArr[i].setType(fileRef.getType());
@@ -104,15 +104,15 @@ public class CaGeneFilterServiceContextResource extends CaGeneFilterServiceConte
 		  throw new RemoteException("setDataInput cause exception.  Detail: " + ew.getMessage(), ew);
 	  }
 	}
-
-
-
-
+	
+	
+	
+	
 	public org.bioconductor.cagrid.data.TwoChannelExpressionDataCollection fileRecode(final org.bioconductor.cagrid.cagenefilter.SpotQualityRecode sqRecode,
 			                                                                           final org.bioconductor.rserviceJms.services.caGeneFilter.caGeneFilter caGeneFilter) throws Exception
 	{
 		try {
-
+						
 			// if the status is in processing, sleep for 3 secs and check on status again:
 			int waiting_time = 0;
 		  	int sleep_time = 3000;
@@ -130,36 +130,38 @@ public class CaGeneFilterServiceContextResource extends CaGeneFilterServiceConte
 					throw ew;
 				}
 			}
-
+			
+			// create caGeneFilter instance to make a call			 
 			FileReferences fileReferences = new FileReferences(m_fromClientFileReferenceList);
 			org.bioconductor.packages.rservices.RJFileReferences rjFileRef = new org.bioconductor.packages.rservices.RJFileReferences(fileReferences);
-
+			
 			// mapping cagrid SpotQualityRecode to R-caGeneFilter package SpotQualityRecode:
-			org.bioconductor.packages.caGeneFilter.SpotQualityRecode biocSpotQRecode =
+			org.bioconductor.packages.caGeneFilter.SpotQualityRecode biocSpotQRecode = 
 				           new org.bioconductor.packages.caGeneFilter.SpotQualityRecode(new String[]{sqRecode.getColumnIdentifier()},
 				        		                                                        new double[]{sqRecode.getColumnMinimumThresholdValue()},
 				        		                                                        new double[]{sqRecode.getRecodeValue()});
-
+			
+			// this returned object array should have length of 2, one for each channel: red & green.
 			Object[] returnedObjArr = caGeneFilter.caFileGeneRecode(rjFileRef, biocSpotQRecode);
-
+			
 			if(returnedObjArr == null || returnedObjArr.length == 0) {
 				// return an empty object
 				return new org.bioconductor.cagrid.data.TwoChannelExpressionDataCollection();
 			}
-
+			
 			return this.convertToExpressionDataCollection(returnedObjArr);
 		}
-		catch(Exception ew) {
+		catch(Exception ew) {			
 			throw ew;
 		}
 	}
-
+	
 	private org.bioconductor.cagrid.data.TwoChannelExpressionDataCollection convertToExpressionDataCollection(final Object[] returnedObjArr) throws Exception
 	{
 		try {
 			RJNumericMatrix redDataCollection = (RJNumericMatrix)returnedObjArr[0];
 			RJNumericMatrix greenDataCollection = (RJNumericMatrix)returnedObjArr[1];
-
+			
 			return CaGeneFilterHelper.convertToTwoChannelExpressionDataCollection(redDataCollection, greenDataCollection);
 		}
 		catch(ClassCastException ew) {
@@ -169,13 +171,6 @@ public class CaGeneFilterServiceContextResource extends CaGeneFilterServiceConte
 			throw ew;
 		}
 	}
-
-	/*
-	 * Implementation part for HelperServiceContextResource interface
-	 */
-	public String identifyContextSelf()
-	{
-		return ("NNNN: I am  CaGeneFilterContextResource...");
-	}
-
+	
+	
 }
